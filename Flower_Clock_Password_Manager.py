e8a4148fab81e1ee00647f5122a9db6b2d8b4c7b
zhr-ab <zhr0624@outlook.com>
2025-06-20 18:26:38 +0800

    添加项目文件。

---
+++ Flower_Clock_Password_Manager.py
@@ -0,0 +1,18 @@
+﻿import pgzrun
+import pygame
+
+# 设置窗口宽高
+WIDTH = 1000
+HEIGHT = 600
+
+# 设置窗口标题
+TITLE = "Flower Clock Password Manager"
+
+# 设置窗口图标
+#icon = pygame.image.load('icon.png')  # 替换为你的图标文件路径
+#pygame.display.set_icon(icon)
+
+def draw():
+    screen.clear()
+
+pgzrun.go()
