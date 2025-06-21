f3d6609bffc903288d63283795ba9a36ab5191f0
zhr-ab <zhr0624@outlook.com>
2025-06-21 09:50:54 +0800

    new

--- Flower_Clock_Password_Manager.py
+++ Flower_Clock_Password_Manager.py
@@ -7,10 +7,10 @@ HEIGHT = 600
 
 # 设置窗口标题
 TITLE = "Flower Clock Password Manager"
-
-# 设置窗口图标
-#icon = pygame.image.load('icon.png')  # 替换为你的图标文件路径
-#pygame.display.set_icon(icon)
+def update():
+    # 设置窗口图标
+    icon = pygame.image.load(r"images\Flower_Clock_Password_Manager.ico")
+    pygame.display.set_icon(icon)
 
 def draw():
     screen.clear()
